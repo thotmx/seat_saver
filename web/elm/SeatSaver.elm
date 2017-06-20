408adc58e759f5e3f4e7e3d6fc3195b511148f37
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2017-06-19 23:45:43 -0500

    Tutorial part 4. Render simple seat list

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,10 +1,40 @@
 module SeatSaver exposing (..)
 
-import Html
+import Html exposing (ul, li, text)
+import Html.Attributes exposing (class)
 
 main =  
-  view
+  view init
+
+-- MODEL 
+
+type alias Seat = 
+  { seatNumber : Int
+  , occupied : Bool
+  }
+
+type alias Model =
+  List Seat
+
+init = 
+  [ { seatNumber = 1, occupied = False }
+  , { seatNumber = 2, occupied = False }
+  , { seatNumber = 3, occupied = False }
+  , { seatNumber = 4, occupied = False }
+  , { seatNumber = 5, occupied = False }
+  , { seatNumber = 6, occupied = False }
+  , { seatNumber = 7, occupied = False }
+  , { seatNumber = 8, occupied = False }
+  , { seatNumber = 9, occupied = False }
+  , { seatNumber = 10, occupied = False }
+  , { seatNumber = 11, occupied = False }
+  , { seatNumber = 12, occupied = False }
+  ]
+
 
 -- VIEW
-view = 
-  Html.text "Woo hoo, I'm in a view"
+view model  = 
+  ul [ class "seats" ] (List.map seatItem model) 
+
+seatItem seat = 
+  li [ class "seat available" ] [ text (toString seat.seatNumber) ] 
