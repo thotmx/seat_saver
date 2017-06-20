4a4cf1c202af31d4195db69728aac4135e9e8b69
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2017-06-19 21:55:24 -0500

    Tutorial Part 2: Include elm into phoenix application

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule SeatSaver.PageControllerTest do
   use SeatSaver.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+      conn = get conn, "/"
+      assert html_response(conn, 200) =~ "<div id=\"elm-main\"></div>"
   end
 end
